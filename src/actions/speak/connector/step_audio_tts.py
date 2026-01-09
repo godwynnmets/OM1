e716c31648a57bbb02f55802a3e2d82bdb0185ef
godwin metsagharun <37663013+godwynnmets@users.noreply.github.com>
2026-01-09 16:51:01 +0100

    Update step_audio_tts.py

--- src/actions/speak/connector/step_audio_tts.py
+++ src/actions/speak/connector/step_audio_tts.py
@@ -9,8 +9,7 @@ from pydantic import Field
 
 from actions.base import ActionConfig, ActionConnector
 from actions.speak.interface import SpeakInput
-from providers.asr_rtsp_provider import ASRRTSPProvider
-from providers.elevenlabs_tts_provider import ElevenLabsTTSProvider
+from providers.step_audio_tts_provider import StepAudioTTSProvider
 from providers.io_provider import IOProvider
 from providers.teleops_conversation_provider import TeleopsConversationProvider
 from zenoh_msgs import (
@@ -30,6 +29,7 @@ class SpeakStepAudioTTSConfig(ActionConfig):
     Parameters:
     ----------
     """
+    pass
 
 
 class SpeakStepAudioTTSConnector(
@@ -51,6 +51,21 @@ class SpeakStepAudioTTSConnector(
         """
         super().__init__(config)
 
+        # OM API key
+        api_key = getattr(self.config, "api_key", None)
+
+        # IO Provider
+        self.io_provider = IOProvider()
+
+        # Initialize Step Audio TTS Provider
+        self.tts = StepAudioTTSProvider()
+        self.tts.start()
+
+        # TTS status
+        self.tts_enabled = True
+
+        # Initialize conversation provider
+        self.conversation_provider = TeleopsConversationProvider(api_key=api_key)
 
     async def connect(self, output_interface: SpeakInput) -> None:
         """
@@ -61,10 +76,26 @@ class SpeakStepAudioTTSConnector(
         output_interface : SpeakInput
             The SpeakInput interface containing the text to be spoken.
         """
-        pass
+        if self.tts_enabled is False:
+            logging.info("TTS is disabled, skipping TTS action")
+            return
+
+        # Add pending message to TTS
+        pending_message = self.tts.create_pending_message(output_interface.action)
+
+        # Store robot message to conversation history only if there was ASR input
+        if (
+            self.io_provider.llm_prompt is not None
+            and "INPUT: Voice" in self.io_provider.llm_prompt
+        ):
+            self.conversation_provider.store_robot_message(output_interface.action)
+
+        self.tts.add_pending_message(pending_message)
+
 
     def stop(self) -> None:
         """
         Stop the StepAudio TTS connector and cleanup resources.
         """
-        pass
+        if self.tts:
+            self.tts.stop()
